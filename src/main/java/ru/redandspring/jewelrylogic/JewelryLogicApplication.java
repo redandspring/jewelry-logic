32e3c4493056210701271d163fb610b4df5eb0fb
a.tretyakov <a.tretyakov@platformaofd.ru>
2019-07-09 13:15:07 +0300

    Initial commit

---
+++ src/main/java/ru/redandspring/jewelrylogic/JewelryLogicApplication.java
@@ -0,0 +1,13 @@
+package ru.redandspring.jewelrylogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelryLogicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/redandspring/jewelrylogic/service/GreetService.java
@@ -0,0 +1,8 @@
+package ru.redandspring.jewelrylogic.service;
+
+public class GreetService {
+
+    public static void makeGreet(){
+        System.out.println("Greet");
+    }
+}
